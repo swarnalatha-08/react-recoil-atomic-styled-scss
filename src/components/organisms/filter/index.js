3e2c152eb427819793f07b03bd7f166b708ff5f0
swarnalatha <swarnalatha@swarnalathas-MacBook-Pro.local>
2023-04-10 11:03:35 +0530

    checking after importing existing project to new git repo

--- src/components/organisms/filter/index.js
+++ src/components/organisms/filter/index.js
@@ -7,7 +7,7 @@ export default function Filter() {
     const name = useRecoilValue(activeUser);
   return (
     <>
-      <Heading fontSize="28px" textAlign="start" p="16px" h={"10%"}>
+      <Heading fontSize="28px" textAlign="start" p="16px" h={"12%"}>
         {name}
       </Heading>
       <div style={{ height: "100px" }}>
